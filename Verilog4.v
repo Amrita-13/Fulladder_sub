4c7a00fb943f4400ad3c943570dbbb0f0597b935
Amrita-13 <12amritabs@gmail.com>
2025-12-09 21:33:03 +0530

    Add files via upload

---
+++ Verilog4.v
@@ -0,0 +1,7 @@
+module Verilog4(
+    input  wire a, b, bin,       
+    output wire diff, borrow     
+);
+    assign diff   = a ^ b ^ bin;                  
+    assign borrow = (~a & b) | (~(a ^ b) & bin);  
+endmodule
